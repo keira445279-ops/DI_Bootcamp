6d29a0a56ec8f7dc5ebd808c05ec91b3428bafb4
keira445270-ops <keira445279@gmail.com>
2025-12-14 12:44:13 +0200

    added files

---
+++ Week7_6th_week_of_learning/Day2/test.sql
@@ -0,0 +1,86 @@
+--Test
+
+--HOW TO CREATE A TABLE
+
+-- CREATE TABLE actors(
+-- actors_id SERIAL PRIMARY KEY,
+-- first_name VARCHAR(50) NOT NULL,
+-- last_name VARCHAR(100) NOT NULL,
+-- birth_day DATE NOT NULL,
+-- number_oscars SMALLINT
+-- )
+
+
+--HOW TO INSERT DATA
+
+-- INSERT INTO actors(first_name, last_name, birth_day, number_oscars)
+-- VALUES ('Matt', 'Damon', '08/10/1970', 5) 
+
+-- INSERT INTO actors(first_name, last_name, birth_day, number_oscars)
+-- VALUES ('Julianne', 'Moore', '03/12/1960', 1) 
+
+-- INSERT INTO actors(first_name, last_name, birth_day, number_oscars)
+-- VALUES ('Helen', 'Mirren', '26/07/1945', 1) 
+
+-- INSERT INTO actors (first_name, last_name, birth_day, number_oscars)
+-- VALUES
+-- 	('Colin', 'Firth', '10/09/1960', 1),
+-- 	('Nicolas', 'Cage', '07/01/1964', 1),
+-- 	('Meryl', 'Streep', '22/07/1949', 3),
+-- 	('Jodie', 'Foster', '19/11/1962', 2),
+-- 	('Emma', 'Stone', '06/11/1988', 2)
+
+-- DELETE FROM actors
+-- WHERE first_name = 'Matt' AND last_name = 'Mirren'
+
+-- INSERT INTO actors(first_name, last_name, birth_day, number_oscars)
+-- VALUES ('Natalie', 'Portman', '09/06/1981', NULL) 
+
+
+--DIFFERENT WAYS OF RETRRIEVING THE DATA
+-- SELECT first_name, number_oscars
+-- FROM actors
+
+-- SELECT *
+-- FROM actors
+-- WHERE number_oscars > 2
+
+-- SELECT *
+-- FROM actors
+-- WHERE number_oscars = 2 AND first_name = 'Jodie'
+
+-- SELECT *
+-- FROM actors
+-- WHERE (number_oscars = 2 AND first_name = 'Jodie') AND last_name = 'Foster'
+
+-- SELECT *
+-- FROM actors
+-- WHERE number_oscars IS NULL
+
+-- SELECT first_name
+-- FROM actors
+-- WHERE last_name LIKE '%p' --ends
+
+-- SELECT first_name
+-- FROM actors
+-- WHERE last_name ILIKE 's%' --starts
+
+-- SELECT *
+-- FROM actors
+-- WHERE number_oscars > 2 
+-- LIMIT 1
+
+-- SELECT first_name
+-- FROM actors
+-- ORDER BY first_name DESC -- DESC - from high to low
+
+-- DELERE A RECORD
+
+--DELETE FROM actors
+--WHERE actor_id = 6
+
+--ALTER A TABLE (CHANGE)
+-- ALTER TABLE actors
+-- RENAME COLUMN number_oscars TO oscars
+
+-- DROP TABLE IF EXISTS actors
